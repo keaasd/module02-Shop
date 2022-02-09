03a750119b803ee03fe88fb623e7e2ae0278f82e
“Евгений <kea-asd@yandex.ru>
2022-02-10 00:35:32 +0700

    Add swiper

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 import 'normalize.css'
 import './styles/main.scss';
+
+import $ from 'jquery';
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+// Swiper свайпер
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});

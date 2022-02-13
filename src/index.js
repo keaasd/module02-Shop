abce3f347e8b9f73fd39bea7c8746d8fceca4821
“Евгений <kea-asd@yandex.ru>
2022-02-14 00:09:27 +0700

    Add slide 2

--- src/index.js
+++ src/index.js
@@ -27,3 +27,20 @@ var swiper = new Swiper(".offerSwiper", {
     },
     modules: [Navigation, Pagination],
 });
+// trands
+var swiper = new Swiper(".trandsSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    modules: [Navigation, Pagination],
+});
